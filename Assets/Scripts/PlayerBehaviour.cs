0b4015a82b633b88ce375878d3f39544f28b8edd
Ugur <ugurari23@gmail.com>
2023-05-22 21:52:32 +0300

    obstacle models added - obstacle animations added - kitten model added, kitten sleep animation added - level 1 updated - cat bed added

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -9,10 +9,13 @@ public class PlayerBehaviour : MonoBehaviour
     [SerializeField] private PlayerMovement playerMovement;
     private Vector3 initialPosition;
     private Quaternion initialRotation;
+    private bool isDead;
+
     private void Awake()
     {
         initialPosition = transform.position;
         initialRotation = transform.rotation;
+        isDead = false;
     }
 
     public void OnTriggerEnter(Collider other)
@@ -23,11 +26,12 @@ public class PlayerBehaviour : MonoBehaviour
             other.gameObject.SetActive(false);
             
         }
-        else if (other.gameObject.CompareTag("Obstacle"))
+        else if (other.gameObject.CompareTag("Obstacle") && isDead==false)
         {
             animatorController.PlayDeath();
             playerMovement.enabled = false;
             StartCoroutine(Respawn());
+            isDead = true;
 
                
         }
@@ -37,6 +41,7 @@ public class PlayerBehaviour : MonoBehaviour
     {
         yield return new WaitForSeconds(3);
         SceneManager.LoadScene(0);
+        isDead = false;
         //transform.position = initialPosition;
         //transform.rotation = initialRotation;
         //animatorController.PlayIdle();
